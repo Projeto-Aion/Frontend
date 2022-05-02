6ebd35e42d0ae35974a6c87c1ea0b0c7bb3a948b
bonflorrane <bonfim_lorrane@hotmail.com>
2022-05-02 16:07:10 -0300

    update quem-somos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuemSomosComponent
   ],
   imports: [
     BrowserModule,

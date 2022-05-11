cb3c9da5e004c7e1df77f8df965c9859981eb1f8
Kaua Mauricio <kauacariocaktd@gmail.com>
2022-05-11 10:48:54 -0300

    CRUD Postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { environment } from 'src/environments/environment.prod';
+import { Observable } from 'rxjs';;
 import { UsuarioLogin } from '../model/UsuarioLogin';
 import { Usuario } from '../model/Usuario';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
   constructor(
     private http: HttpClient
   ) { }
@@ -21,6 +21,20 @@ export class AuthService {
 
   logar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<Usuario>('https://projeto-aion.herokuapp.com/usuarios/logar', usuarioLogin)
+  }
+
+  getByIdUser(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`http://localhost:8080/usuarios/${id}`)
+  }
+
+  logado() {
+    let ok: boolean = false
+
+    if(environment.token != '') {
+      ok = true
+    }
+
+    return ok
   }
 
 }

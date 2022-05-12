8ab7bb621d619df316fa262a0c039b13da96fecd
Jokyeon <jhonathan.fs@hotmail.com>
2022-05-12 15:54:31 -0300

    botões tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
@@ -14,7 +16,9 @@ const routes: Routes = [
   {path:'rodape', component: RodapeComponent},
   {path:'quemsomos', component: QuemSomosComponent},
   {path:'tema', component: TemaComponent},
-  {path:'inicio', component: InicioComponent}
+  {path:'inicio', component: InicioComponent},
+  {path:'temaEdit/:id', component: TemaEditComponent},
+  {path:'temaDelete/:id', component: TemaDeleteComponent},
 ];
 
 @NgModule({

ce464593759fef31fc88f4ae941a7532b5b52504
Joao <jp11.ls@hotmail.com>
2022-05-12 23:05:59 -0300

    tratativa de erro

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -1,14 +1,14 @@
-import { Tema } from "./Tema";
-import { Usuario } from "./Usuario";
+import { Tema } from "./Tema"
+import { Usuario } from "./Usuario"
 
 export class Postagem{
-    public id: number;
-    public titulo: string;
-    public descricao: string;
-    public data: Date;
-    public usuario:Usuario;
-    public tema: Tema;
-    public local: string;
+    public id: number
+    public titulo: string
+    public descricao: string
+    public data: Date
+    public usuario:Usuario
+    public tema: Tema
+    public local: string
     public anexo: string
   }
 

d4a934ebcc1453895dac3c8129b142f3e6a272d4
Jokyeon <jhonathan.fs@hotmail.com>
2022-05-18 19:45:13 -0300

    feed perfil

--- src/app/perfil-corporativo/perfil-corporativo.component.ts
+++ src/app/perfil-corporativo/perfil-corporativo.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-perfil-corporativo',
@@ -10,13 +8,9 @@ import { environment } from 'src/environments/environment.prod';
 export class PerfilCorporativoComponent implements OnInit {
 
   constructor(
-    private router: Router
   ) { }
 
   ngOnInit(){
-    if(environment.token == '') {          //SE EU DER UM ATUALIZAR TEM QUE VOLTAR PRO LOGIN
-      this.router.navigate(['/login'])
-    }
   }
 
 }
